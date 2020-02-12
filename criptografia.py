075c82090ba13562c0b3b3896cd76ac5bea8bfdc
Brayan Llanos <brayan0428@gmail.com>
2020-02-11 21:03:53 -0500

    Algoritmo de cifrado

---
+++ criptografia.py
@@ -0,0 +1,122 @@
+KEYS = {
+    'a': 'w',
+    'b': 'E',
+    'c': 'x',
+    'd': '1',
+    'e': 'a',
+    'f': 't',
+    'g': '0',
+    'h': 'C',
+    'i': 'b',
+    'j': '!',
+    'k': 'z',
+    'l': '8',
+    'm': 'M',
+    'n': 'I',
+    'o': 'd',
+    'p': '.',
+    'q': 'U',
+    'r': 'Y',
+    's': 'i',
+    't': '3',
+    'u': ',',
+    'v': 'J',
+    'w': 'N',
+    'x': 'f',
+    'y': 'm',
+    'z': 'W',
+    'A': 'G',
+    'B': 'S',
+    'C': 'j',
+    'D': 'n',
+    'E': 's',
+    'F': 'Q',
+    'G': 'o',
+    'H': 'e',
+    'I': 'u',
+    'J': 'g',
+    'K': '2',
+    'L': '9',
+    'M': 'A',
+    'N': '5',
+    'O': '4',
+    'P': '?',
+    'Q': 'c',
+    'R': 'r',
+    'S': 'O',
+    'T': 'P',
+    'U': 'h',
+    'V': '6',
+    'W': 'q',
+    'X': 'H',
+    'Y': 'R',
+    'Z': 'l',
+    '0': 'k',
+    '1': '7',
+    '2': 'X',
+    '3': 'L',
+    '4': 'p',
+    '5': 'v',
+    '6': 'T',
+    '7': 'V',
+    '8': 'y',
+    '9': 'K',
+    '.': 'Z',
+    ',': 'D',
+    '?': 'F',
+    '!': 'B',
+}
+
+def cypher(message):
+    words = message.split(' ')
+    letters_cypher = []
+    for word in words:
+      for letter in word:
+        letters_cypher.append(KEYS[letter])
+      letters_cypher.append(' ')
+    return ''.join(letters_cypher)
+
+
+
+def decipher(message):
+    words = message.split(' ')
+    letters_decipher = []
+    for word in words:
+      for letter in word:
+        for key,value in KEYS.items():
+          if value == letter:
+            letters_decipher.append(key)
+      letters_decipher.append(' ')
+    return ''.join(letters_decipher)
+
+
+def run():
+
+    while True:
+
+        command = str(input('''--- * --- * --- * --- * --- * --- * --- * ---
+
+            Bienvenido a criptografía. ¿Qué deseas hacer?
+
+            [c]ifrar mensaje
+            [d]ecifrar mensaje
+            [s]alir
+        '''))
+
+        if command == 'c':
+            word = str(input("Ingrese la palabra a crifrar: "))
+            result = cypher(word)
+            print(result)
+        elif command == 'd':
+            word = str(input("Ingrese el codigo a descrifrar: "))
+            result = decipher(word)
+            print(result)
+        elif command == 's':
+            
+        else:
+            print('¡Comando no encontrado!')
+
+
+if __name__ == '__main__':
+    print('M E N S A J E S  C I F R A D O S')
+    run()
